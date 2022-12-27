8d892fb766c2f0396ea2c78d59f42db94ae251be
Sumeye <dalkilincsumeye@gmail.com>
2022-12-27 10:54:36 -0500

    JDZH5-208 "SecurityServiceImpl" class and User Repository Interface are created

---
+++ src/main/java/djrAccounting/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package djrAccounting.repository;
+
+import djrAccounting.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+
+
+}
