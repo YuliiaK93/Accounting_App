fe03f1dbfdeeeb3ef11c2ffd359799247047ed88
iskenderColak <iclk84@gmail.com>
2022-12-28 10:29:53 +0300

    JDZH5-196 CompanyDtoConverter class added

---
+++ src/main/java/djrAccounting/converter/CompanyDtoConverter.java
@@ -0,0 +1,26 @@
+package djrAccounting.converter;
+
+import djrAccounting.dto.CompanyDto;
+import djrAccounting.service.CompanyService;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CompanyDtoConverter implements Converter<String, CompanyDto> {
+
+    private final CompanyService companyService;
+
+    public CompanyDtoConverter(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @Override
+    public CompanyDto convert(String source) {
+
+        if (source == null || source.isBlank()) {
+            return null;
+        }
+
+        return companyService.findById(Long.parseLong(source));
+    }
+}
