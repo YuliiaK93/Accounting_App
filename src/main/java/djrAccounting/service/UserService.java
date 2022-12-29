b14012444a0403b5dccef21c62c2b6a30d05197d
Yuliia <yulia.karnoza@gmail.com>
2022-12-29 14:04:59 -0500

    JDZH5-230 - Implementing save method

--- src/main/java/djrAccounting/service/UserService.java
+++ src/main/java/djrAccounting/service/UserService.java
@@ -4,9 +4,14 @@ package djrAccounting.service;
 import djrAccounting.dto.UserDto;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface UserService {
 
     UserDto findById(Long id);
+    UserDto findByUsername(String username);
+    List<UserDto> listAllUsers();
+    void save(UserDto user);
 }
 
