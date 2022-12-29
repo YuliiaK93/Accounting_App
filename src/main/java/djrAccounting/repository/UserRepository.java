e991c7c4285ecdc01c689b26813f2fac8df0d63a
Yuliia <yulia.karnoza@gmail.com>
2022-12-29 14:24:45 -0500

    JDZH5-230 - findByCompanyId and findByRoleId added

--- src/main/java/djrAccounting/repository/UserRepository.java
+++ src/main/java/djrAccounting/repository/UserRepository.java
@@ -5,12 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
     User findByUsername(String username);
+    List<User> findByCompanyId(Long id);
+    List<User> findByRoleId(Long id);
+
 
 
 
--- src/main/java/djrAccounting/service/implementation/UserServiceImpl.java
+++ src/main/java/djrAccounting/service/implementation/UserServiceImpl.java
@@ -1,22 +1,45 @@
 package djrAccounting.service.implementation;
 
 import djrAccounting.dto.UserDto;
+import djrAccounting.entity.User;
 import djrAccounting.mapper.MapperUtil;
 import djrAccounting.repository.UserRepository;
+import djrAccounting.service.SecurityService;
 import djrAccounting.service.UserService;
+
+import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final MapperUtil mapperUtil;
 
-    public UserServiceImpl(UserRepository userRepository, MapperUtil mapperUtil) {
+    private final SecurityService securityService;
+
+    public UserServiceImpl(UserRepository userRepository, MapperUtil mapperUtil, SecurityService securityService) {
         this.userRepository = userRepository;
         this.mapperUtil = mapperUtil;
+        this.securityService = securityService;
     }
 
     @Override
     public UserDto findById(Long id) {
         return mapperUtil.convert(userRepository.findById(id).orElseThrow(), UserDto.class);
+    }
+
+    @Override
+    public UserDto findByUsername(String username) {
+        User user = userRepository.findByUsername(username).orElseThrow(() ->;
+        return mapperUtil.convert(user, new UserDto());
+    }
+
+    @Override
+    public List<UserDto> listAllUsers() {
+        return null;
+    }
+
+    @Override
+    public void save(UserDto user) {
+
     }
 }
