d435c8c90482d19975d5236ed00f6f836ad16639
iskenderColak <iclk84@gmail.com>
2022-12-30 23:35:48 +0300

    JDZH5-227-Extension1 Company create and update fixes

--- src/main/java/djrAccounting/dto/CompanyDto.java
+++ src/main/java/djrAccounting/dto/CompanyDto.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @AllArgsConstructor
@@ -28,10 +27,8 @@ public class CompanyDto {
     @NotBlank(message = "Required field")
     private String website;
 
-    @NotNull(message = "Required field")
     private CompanyStatus companyStatus;
 
-    @NotNull(message = "Required field")
     @Valid
     private AddressDto address;
 }
