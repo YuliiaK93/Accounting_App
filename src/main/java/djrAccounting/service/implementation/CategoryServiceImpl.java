0ed3787826d73bbd50d4e4c31d64fe743e02cf6d
Sumeye <dalkilincsumeye@gmail.com>
2022-12-29 23:56:28 -0500

    JDZH5-239 - listAllCategories method is overrided inside the CategoryServiceImpl

--- src/main/java/djrAccounting/service/implementation/CategoryServiceImpl.java
+++ src/main/java/djrAccounting/service/implementation/CategoryServiceImpl.java
@@ -1,10 +1,13 @@
 package djrAccounting.service.implementation;
 
 import djrAccounting.dto.CategoryDto;
+import djrAccounting.entity.Category;
 import djrAccounting.mapper.MapperUtil;
 import djrAccounting.repository.CategoryRepository;
 import djrAccounting.service.CategoryService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -22,5 +25,11 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public CategoryDto findById(Long id) {
         return mapper.convert(categoryRepository.findById(id).orElseThrow(),CategoryDto.class);
+    }
+
+    @Override
+    public List<Category> listAllCategories() {
+        return null;
+
     }
 }
