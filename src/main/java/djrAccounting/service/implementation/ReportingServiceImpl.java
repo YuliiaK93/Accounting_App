ecb92503ae0dba2e11deed6c045ca1aee15cac24
Yzabakli <abakliyusufziya@gmail.com>
2022-12-30 14:46:37 +0100

    ReportingServiceImpl getAllProfitLossPerMonth method completed

--- src/main/java/djrAccounting/service/implementation/ReportingServiceImpl.java
+++ src/main/java/djrAccounting/service/implementation/ReportingServiceImpl.java
@@ -1,17 +1,14 @@
 package djrAccounting.service.implementation;
 
 import djrAccounting.dto.InvoiceProductDto;
-import djrAccounting.enums.InvoiceType;
 import djrAccounting.service.InvoiceProductService;
 import djrAccounting.service.ReportingService;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.time.LocalDate;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class ReportingServiceImpl implements ReportingService {
@@ -31,44 +28,12 @@ public class ReportingServiceImpl implements ReportingService {
     @Override
     public Map<String, BigDecimal> getAllProfitLossPerMonth() {
 
-        Map<String, BigDecimal> map = new LinkedHashMap<>();
-
-        invoiceProductService.getAllByInvoiceStatusApprovedForCurrentCompany().forEach(invoiceProductDto -> {
-
-            LocalDate date = invoiceProductDto.getInvoice().getDate();
-            String key = date.getYear() + " " + date.getMonth();
-            BigDecimal priceWithTax = invoiceProductDto.getPrice()
-                    .add(invoiceProductDto.getPrice()
-                            .multiply(BigDecimal.valueOf(invoiceProductDto.getTax()))
-                            .divide(BigDecimal.valueOf(100), RoundingMode.HALF_EVEN))
-                    .multiply(BigDecimal.valueOf(invoiceProductDto.getQuantity()));
-
-            if (invoiceProductDto.getInvoice().getInvoiceType().equals(InvoiceType.SALES)){
-
-                map.put(key, map.getOrDefault(key, BigDecimal.ZERO).add(priceWithTax));
-
-            }else {
-
-                map.put(key, map.getOrDefault(key, BigDecimal.ZERO).subtract(priceWithTax));
-            }
-        });
-
-        return map;
+        return invoiceProductService.getAllByInvoiceStatusApprovedForCurrentCompany()
+                .stream()
+                .collect(Collectors.toMap(invoiceProductDto -> invoiceProductDto.getInvoice()
+                        .getDate()
+                        .getYear() + " " + invoiceProductDto.getInvoice()
+                        .getDate()
+                        .getMonth(), InvoiceProductDto::getProfitLoss, (BigDecimal::add)));
     }
-
-//    @Override
-//    public Map<String, BigDecimal> getAllProfitLossPerMonth() {
-//
-//        Map<String, BigDecimal> map = new LinkedHashMap<>();
-//
-//        invoiceProductService.getAllByInvoiceStatusApprovedForCurrentCompany().forEach(invoiceProductDto -> {
-//
-//                    LocalDate date = invoiceProductDto.getInvoice().getDate();
-//                    String key = date.getYear() + " " + date.getMonth();
-//
-//                    map.put(key, map.getOrDefault(key, BigDecimal.ZERO).add(invoiceProductDto.getProfitLoss()));
-//                });
-//
-//        return map;
-//    }
 }
