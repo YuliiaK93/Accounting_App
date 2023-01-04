90ffd9bb8e523717c0d4834c1cf47351063c7378
Yzabakli <abakliyusufziya@gmail.com>
2023-01-04 01:43:47 +0100

    created Test for these methods DashboardServiceImpl: financialSummaryForCurrentCompany
    InvoiceProductServiceImpl:
    getTotalPriceByInvoice
    getTotalPriceWithTaxByInvoice
    getTotalCostForCurrentCompany
    getTotalSalesForCurrentCompany
    getTotalProfitLossForCurrentCompany
    
    InvoiceServiceImpl:
    getLast3ApprovedInvoicesForCurrentUserCompany
    
    ReportingServiceImpl:
    getAllInvoiceProductsThatApprovedFroCurrentCompany
    getAllProfitLossPerMonth

--- src/test/java/djrAccounting/unit/service/implementation/DashboardServiceImplTest.java
+++ src/test/java/djrAccounting/unit/service/implementation/DashboardServiceImplTest.java
@@ -2,7 +2,6 @@ package djrAccounting.unit.service.implementation;
 
 import djrAccounting.client.ExchangeClient;
 import djrAccounting.dto.dashboard.FinancialSummaryDto;
-import djrAccounting.service.DashboardService;
 import djrAccounting.service.InvoiceProductService;
 import djrAccounting.service.implementation.DashboardServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,13 +10,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class DashboardServiceImplTest {
 
-    private DashboardService dashboardService;
+    private DashboardServiceImpl dashboardService;
 
     @Mock
     private InvoiceProductService invoiceProductService;
@@ -27,18 +28,20 @@ class DashboardServiceImplTest {
 
     @BeforeEach
     void setUp() {
-
         dashboardService = new DashboardServiceImpl(invoiceProductService, exchangeClient);
     }
 
     @Test
     void financialSummaryForCurrentCompany() {
 
-
-        when(invoiceProductService.getTotalSalesForCurrentCompany());
+        when(invoiceProductService.getTotalCostForCurrentCompany()).thenReturn(BigDecimal.TEN);
+        when(invoiceProductService.getTotalSalesForCurrentCompany()).thenReturn(BigDecimal.ONE);
+        when(invoiceProductService.getTotalProfitLossForCurrentCompany()).thenReturn(BigDecimal.ZERO);
 
         FinancialSummaryDto financialSummaryDto = dashboardService.financialSummaryForCurrentCompany();
 
-
+        assertEquals(BigDecimal.TEN, financialSummaryDto.getTotalCost());
+        assertEquals(BigDecimal.ONE, financialSummaryDto.getTotalSales());
+        assertEquals(BigDecimal.ZERO, financialSummaryDto.getProfitLoss());
     }
 }
