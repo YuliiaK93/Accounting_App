41cabcef8c892f6901d79b93848ec83a911efa1f
Ekaterina <ekaterina.sementeva@gmail.com>
2023-01-09 22:43:13 -0500

    JDZH5-299 Test delete and booleans

--- src/test/java/djrAccounting/unit/service/implementation/ClientVendorServiceImplTest.java
+++ src/test/java/djrAccounting/unit/service/implementation/ClientVendorServiceImplTest.java
@@ -21,10 +21,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
@@ -45,26 +42,141 @@ class ClientVendorServiceImplTest {
 
     @Test
     void findById_validInput_Test() {
-        //Given Client vendor in DB with a particular ID
         ClientVendor clientVendor = new ClientVendor();
         clientVendor.setId(1L);
-        //When search in DB get ID you get Optional of clientVendor
+
         when(clientVendorRepository.findById(1L)).thenReturn(Optional.of(clientVendor));
-        //You expect the return of method to bring dto with same id
+
         ClientVendorDto expectedClientVendorDto = new ClientVendorDto();
         expectedClientVendorDto.setId(clientVendor.getId());
-        // convert clientVendor to Dto, and get expected clientVendor
+
         when(mapperUtil.convert(clientVendor, ClientVendorDto.class)).thenReturn(expectedClientVendorDto);
-        //Possibility for the output
         ClientVendorDto actualClientVendorDto = clientVendorServiceImpl.findById(1L);
-        // Assert
-        assertNotNull(actualClientVendorDto);//returns and object
-        assertEquals(expectedClientVendorDto, actualClientVendorDto);//the Object is exactly the object we need
+
+        assertNotNull(actualClientVendorDto);
+        assertEquals(expectedClientVendorDto, actualClientVendorDto);
     }
 
-@Test
-    void deleteById_Test(){
+    @Test
+    void deleteById_Valid_Test() throws IllegalAccessException {
+        ClientVendor clientVendor = new ClientVendor();
+        clientVendor.setId(1L);
+        clientVendor.setIsDeleted(false);
 
-}
+        when(clientVendorRepository.findById(1L)).thenReturn(Optional.of(clientVendor));
+        when(invoiceService.existsByClientVendorId(1L)).thenReturn(false);
 
+        clientVendorServiceImpl.deleteById(1L);
+
+        verify(clientVendorRepository).findById(1L);
+        verify(invoiceService).existsByClientVendorId(1L);
+        verify(clientVendorRepository).save(clientVendor);
+    }
+
+    @Test
+    public void deleteById_ClientVendorHasInvoices_Exception_Test() {
+        ClientVendor clientVendor = new ClientVendor();
+        clientVendor.setId(1L);
+        clientVendor.setIsDeleted(false);
+
+        when(clientVendorRepository.findById(1L)).thenReturn(Optional.of(clientVendor));
+        when(invoiceService.existsByClientVendorId(1L)).thenReturn(true);
+        assertThrows(IllegalAccessException.class, () -> clientVendorServiceImpl.deleteById(1L));
+
+        verify(clientVendorRepository).findById(1L);
+        verify(invoiceService).existsByClientVendorId(1L);
+    }
+
+    @Test
+    public void deleteById_ClientVendorNotFound_Test() throws IllegalAccessException {
+        when(clientVendorRepository.findById(1L)).thenReturn(Optional.empty());
+        clientVendorServiceImpl.deleteById(1L);
+        verify(clientVendorRepository).findById(1L);
+    }
+
+    @Test
+    void nameExists_Test() {
+        when(clientVendorRepository.existsByClientVendorName(anyString())).thenReturn(true);
+        assertTrue(clientVendorServiceImpl.nameExists(anyString()));
+        verify(clientVendorRepository).existsByClientVendorName(anyString());
+    }
+
+    @Test
+    void nameNotExists_Test() {
+        when(clientVendorRepository.existsByClientVendorName(anyString())).thenReturn(false);
+        assertFalse(clientVendorServiceImpl.nameExists(anyString()));
+        verify(clientVendorRepository).existsByClientVendorName(anyString());
+    }
+
+    @Test
+    void duplicatedName_Test() {
+        ClientVendor clientVendor = new ClientVendor();
+        clientVendor.setId(1L);
+        clientVendor.setClientVendorName("name");
+        ClientVendorDto clientVendorDto = new ClientVendorDto();
+        clientVendorDto.setId(1L);
+        clientVendorDto.setClientVendorName("name");
+
+        when(clientVendorRepository.findById(1L)).thenReturn(Optional.of(clientVendor));
+
+        boolean result = clientVendorServiceImpl.duplicatedName(clientVendorDto);
+        assertFalse(result);
+    }
+
+    @Test
+    void duplicatedName_DifferentName_ReturnsFalse_Test() {
+        ClientVendorDto dto = new ClientVendorDto();
+        dto.setId(1L);
+        dto.setClientVendorName("name");
+
+        ClientVendor cv = new ClientVendor();
+        cv.setId(1L);
+        cv.setClientVendorName("nickname");
+        when(clientVendorRepository.findById(1L)).thenReturn(Optional.of(cv));
+        when(clientVendorServiceImpl.nameExists("name")).thenReturn(false);
+
+        boolean result = clientVendorServiceImpl.duplicatedName(dto);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void duplicatedName_NameMatchFalse_NameExistsTrue_Test() {
+        ClientVendorDto dto = new ClientVendorDto();
+        dto.setId(1L);
+        dto.setClientVendorName("name");
+        ClientVendor cv = new ClientVendor();
+        cv.setId(1L);
+        cv.setClientVendorName("nickname");
+
+        when(clientVendorRepository.findById(1L)).thenReturn(Optional.of(cv));
+        when(clientVendorServiceImpl.nameExists("name")).thenReturn(true);
+
+        boolean result = clientVendorServiceImpl.duplicatedName(dto);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void hasRightToUpdate_Valid_Test() {
+        CompanyDto companyDto = new CompanyDto();
+        companyDto.setId(1L);
+        companyDto.setTitle("company");
+        Company company = new Company();
+        company.setId(1L);
+        company.setTitle("company");
+        UserDto userDto = new UserDto();
+        userDto.setCompany(companyDto);
+
+        when(securityService.getLoggedInUser()).thenReturn(userDto);
+
+        ClientVendor clientVendor = new ClientVendor();
+        clientVendor.setId(1L);
+        clientVendor.setCompany(company);
+
+        when(clientVendorRepository.findById(1L)).thenReturn(Optional.of(clientVendor));
+
+        boolean result = clientVendorServiceImpl.hasRightToUpdate(1L);
+        assertTrue(result);
+    }
 }
