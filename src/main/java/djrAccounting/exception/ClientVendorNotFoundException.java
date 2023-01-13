23e3adf371437aaab4d38dc6fc153cbf38b8813e
Ekaterina <ekaterina.sementeva@gmail.com>
2023-01-12 21:05:05 -0500

    JDZH5-323 ClientVendorNotFoundException

---
+++ src/main/java/djrAccounting/exception/ClientVendorNotFoundException.java
@@ -0,0 +1,8 @@
+package djrAccounting.exception;
+
+public class ClientVendorNotFoundException extends RuntimeException {
+
+    public ClientVendorNotFoundException(String message){
+        super(message);
+    }
+}
